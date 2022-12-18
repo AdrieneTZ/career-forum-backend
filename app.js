ebfa53dbf94cc624aa6b13ff171fee5e3cf73877
AdrieneTZ <tznali17@gmail.com>
2022-12-18 11:50:12 +0800

    feat: add cors and body parser and finish related setup

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const passport = require('./config/passport')
 const routes = require('./routes')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-app.use(passport.initialize())
+app.use(cors())
+app.use(express.urlencoded)
+app.use(express.json())
+app.use(passport.initialize({ extended: true }))
 
 app.use('/api', routes)
 app.listen(port, () => console.log(`This app is listening on port ${port}.`))
