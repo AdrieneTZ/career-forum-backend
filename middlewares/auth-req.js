e1de62a2b0f6657a958b55cdacfad30aedda9032
Sean <107825221+seanlin1125@users.noreply.github.com>
2023-01-12 09:17:35 +0800

    Feature/auth (#6)
    * feat: add answer model and set up relations
    
    * feat: add req-helper
    
    * feat: add file and pagination helpers
    
    * feat: add authentication and authorization rules

--- middlewares/auth-req.js
+++ middlewares/auth-req.js
@@ -1,22 +1,34 @@
 const passport = require('../config/passport')
+const { getUser } = require('../helpers/req-helpers')
 
 const authenticated = (req, res, next) => {
-  const returnUserOrError = (error, user) => {
-    if (error || !user) {
-      return res.status(401).json({ message: 'Unauthorized request' })
-    }
-
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'Unauthorized request' })
     req.user = user
     next()
-  }
-
-  passport.authenticate('jwt', { session: false }, returnUserOrError)(
-    req,
-    res,
-    next
-  )
+  })(req, res, next)
+}
+const authAdmin = (req, res, next) => {
+  if (getUser(req) && getUser(req).isAdmin) return next()
+  return res.status(403).json({ status: 'error', message: 'Unauthorized request' })
+}
+const authRole = (req, res, next) => {
+  if (getUser(req) && getUser(req).role === ('TA' || 'STUDENT' || 'GRADUATE')) return next()
+  return res.status(403).json({ status: 'error', message: 'Unauthorized request' })
+}
+const authApprovalStatus = (req, res, next) => {
+  if (getUser(req) && getUser(req).approvalStatus === 'approved') return next()
+  return res.status(403).json({ status: 'error', message: 'Unauthorized request' })
+}
+const authCurrentUser = (req, res, next) => {
+  if (getUser(req).id === Number(req.params.id)) return next()
+  return res.status(403).json({ status: 'error', message: 'You can only edit your own data' })
 }
 
 module.exports = {
   authenticated,
+  authAdmin,
+  authRole,
+  authApprovalStatus,
+  authCurrentUser
 }
