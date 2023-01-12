75dbfe5752e8772165baf5fed7517a4b5f55cff3
seanlin1125 <sean821125@gmail.com>
2023-01-12 13:10:20 +0800

    feat: add error-handler and multer middleware

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
-
+const upload = require('../../middlewares/multer')
 const userController = require('../../controllers/user-controller')
+const { authCurrentUser } = require('../../middlewares/auth-req')
 
 // 取得當前使用者
 router.get('/current_user', userController.getCurrentUser)
+
+// // 修改個人資料中的頭像
+// router.patch('/:id/avatar', authCurrentUser, upload.fields({ name: 'cover', maxCount: 1 }), userController.patchUserAvatar)
+// // 修改個人資料中的背景圖
+// router.patch('/:id/cover', authCurrentUser, upload.fields({ name: 'cover', maxCount: 1 }), userController.patchUserCover)
+// // 修改個人資料
+// router.put('/:id', authCurrentUser, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), userController.putUserSetting)
 
 module.exports = router
