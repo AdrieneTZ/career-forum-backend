c5ab1f7b6c921cf8eb63587470738571791579e2
seanlin1125 <sean821125@gmail.com>
2023-01-17 19:41:58 +0800

    feat: modify files name to match consistency

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const user = require('./modules/user')
-const question = require('./modules/question')
-// const answer = require('./modules/answer')
-// const admin = require('./modules/admin')
+const users = require('./modules/users')
+const questions = require('./modules/questions')
+// const answers = require('./modules/answers')
+// const admins = require('./modules/admins')
 
 const userController = require('../controllers/user-controller')
 const { authenticated, authPermissionRole } = require('../middlewares/auth-req')
@@ -13,9 +13,9 @@ router.post('/login', userController.login)
 // User register
 router.post('/register', userController.register)
 
-router.use('/users', authenticated, user)
-router.use('/questions', authenticated, question)
-// router.use('/answers', authenticated, answer)
-// router.use('/admin', authenticated, authPermissionRole, admin)
+router.use('/users', authenticated, users)
+router.use('/questions', authenticated, questions)
+// router.use('/answers', authenticated, answers)
+// router.use('/admin', authenticated, authPermissionRole, admins)
 
 module.exports = router
