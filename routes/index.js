889e8fe051694617393e97219cd650a61080b012
Sean <107825221+seanlin1125@users.noreply.github.com>
2023-01-17 19:45:05 +0800

    Feature/middlewares (#11)
    * feat: add answer model and set up relations
    
    * feat: add req-helper
    
    * feat: add file and pagination helpers
    
    * feat: add authentication and authorization rules
    
    * feat: add get current user route
    
    * feat: modify user route, authRole and app version
    
    * feat: add error-handler and multer middleware
    
    * feat: add getUser route
    
    * feat: add post question route
    
    * feat: add get questions route
    
    * modify typo of routes
    
    * feat: modify auth middleware, user-controller and index route
    
    * feat: modify files name to match consistency

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/user-controller')
-const user = require('./modules/user')
-const question = require('./modules/question')
-// const answer = require('./modules/answer')
-// const admin = require('./modules/admin')
+const users = require('./modules/users')
+const questions = require('./modules/questions')
+// const answers = require('./modules/answers')
+// const admins = require('./modules/admins')
 
 const userController = require('../controllers/user-controller')
-const { authenticated, authAdmin } = require('../middlewares/auth-req')
+const { authenticated, authPermissionRole } = require('../middlewares/auth-req')
 
 // User login
 router.post('/login', userController.login)
 // User register
 router.post('/register', userController.register)
 
-router.use('/users', authenticated, user)
-router.use('/questions', authenticated, question)
-// router.use('/answers', authenticated, answer)
-// router.use('/admin', authenticated, authAdmin, admin)
+router.use('/users', authenticated, users)
+router.use('/questions', authenticated, questions)
+// router.use('/answers', authenticated, answers)
+// router.use('/admin', authenticated, authPermissionRole, admins)
 
 module.exports = router
