686c7a6093a066388c16333b25f320d5962f2e1c
seanlin1125 <sean821125@gmail.com>
2023-01-27 14:03:12 +0800

    refactor: add comment for clearTempFile fn

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.use('/', generalErrorHandler) // Error handler
 
 app.listen(port, () => console.log(`This app is listening on port ${port}.`))
 
-setInterval(clearTempFile, 43200000)
+setInterval(clearTempFile, 43200000) // 單位是ms毫秒，相當於半天(12小時)清理一次
 
 module.exports = app
