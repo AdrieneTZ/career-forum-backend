6564e3e041e3c3370529e3f4e4465acc9a4219da
seanlin1125 <sean821125@gmail.com>
2023-01-27 12:51:15 +0800

    refactor: renew imgur and multer to updated method

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const cors = require('cors')
-const passport = require('./config/passport')
-const { generalErrorHandler } = require('./middlewares/error-handler')
-const routes = require('./routes')
-
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
+const cors = require('cors')
+const passport = require('./config/passport')
+const { clearTempFile } = require('./helpers/file-helpers')
+const { generalErrorHandler } = require('./middlewares/error-handler')
+const routes = require('./routes')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 app.use(passport.initialize())
 
 app.use('/api/v1', routes)
-app.use('/', (req, res) => { res.send('Fallback: Oops! Something went wrong.') })
-app.use('/', generalErrorHandler)
+app.use('/', (req, res) => { res.send('Fallback: Oops! Something went wrong.') }) //Fallback
+app.use('/', generalErrorHandler) // Error handler
 
 app.listen(port, () => console.log(`This app is listening on port ${port}.`))
+
+setInterval(clearTempFile, 43200000)
 
 module.exports = app
