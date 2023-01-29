6f589f7ba0d036671a68ba410a4d7af9820a4c5d
seanlin1125 <sean821125@gmail.com>
2023-01-29 21:33:21 +0800

    fix: users route consistent with remote main

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,11 +15,4 @@ router.put('/:id', authCurrentUser, upload.fields([{ name: 'avatar', maxCount: 1
 // 取得單筆使用者
 router.get('/:id', userController.getUser)
 
-// // 修改個人資料中的頭像
-// router.patch('/:id/avatar', authCurrentUser, upload.fields({ name: 'cover', maxCount: 1 }), userController.patchUserAvatar)
-// // 修改個人資料中的背景圖
-// router.patch('/:id/cover', authCurrentUser, upload.fields({ name: 'cover', maxCount: 1 }), userController.patchUserCover)
-// // 修改個人資料
-// router.put('/:id', authCurrentUser, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), userController.putUserSetting)
-
 module.exports = router
