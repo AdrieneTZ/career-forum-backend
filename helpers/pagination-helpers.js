f6ae4b980edf2d3bec7c7b481510c05ac9f1e3bc
seanlin1125 <sean821125@gmail.com>
2023-01-31 13:03:44 +0800

    style: delete pagination example comment

--- helpers/pagination-helpers.js
+++ helpers/pagination-helpers.js
@@ -17,27 +17,3 @@ module.exports = {
   getOffset,
   getPagination
 }
-
-// //使用範例：
-// const { Restaurant, Category } = require('../models')
-// const { getOffset, getPagination } = require('../helpers/pagination-helper')
-// const restaurantServices = {
-//   getRestaurants: (req, cb) => {
-//     const DEFAULT_LIMIT = 9
-//     const categoryId = Number(req.query.categoryId) || ''
-//     const page = Number(req.query.page) || 1
-//     const limit = Number(req.query.limit) || DEFAULT_LIMIT
-//     const offset = getOffset(limit, page)
-//     Promise.all([
-//       Restaurant.findAndCountAll({
-//         include: Category,
-//         where: {
-//           ...categoryId ? { categoryId } : {}
-//         },
-//         limit, //prisma 是 take
-//         offset, // prisma 是 skip
-//         nest: true,
-//         raw: true
-//       }),
-//       Category.findAll({ raw: true })
-//     ])
